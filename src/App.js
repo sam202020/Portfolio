dc4609c22351c4d6f034367d8899f021abd61b1f
Steve <shalomlandsman@gmail.com>
2018-10-18 13:41:07 -0400

    add backend folder, express, cors, and helmet dependencies

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Menu from '@material-ui/core/Menu';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import withRoot from './withRoot';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Menu from "@material-ui/core/Menu";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import withRoot from "./withRoot";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button as ReactstrapButton
+} from "reactstrap";
 
 const styles = theme => ({
   root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
-  },
+    textAlign: "center",
+    paddingTop: theme.spacing.unit * 1
+  }
 });
 
-
 class App extends Component {
-  
   state = {
-    open: false,
+    open: false
   };
 
   handleClose = () => {
     this.setState({
-      open: false,
+      open: false
     });
   };
 
   handleClick = () => {
     this.setState({
-      open: true,
+      open: true
     });
   };
 
@@ -45,6 +57,42 @@ class App extends Component {
 
     return (
       <div className={classes.root}>
+        <Navbar color="inverse" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  Github
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Jumbotron>
+          <Container>
+            <Row>
+              <Col>
+                <h1>Welcome to React</h1>
+                <p>
+                  <ReactstrapButton
+                    tag="a"
+                    color="success"
+                    size="large"
+                    href="http://reactstrap.github.io"
+                    target="_blank"
+                  >
+                    View Reactstrap Docs
+                  </ReactstrapButton>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
         <Dialog open={open} onClose={this.handleClose}>
           <DialogTitle>Super Secret Password</DialogTitle>
           <DialogContent>
@@ -62,7 +110,11 @@ class App extends Component {
         <Typography variant="subtitle1" gutterBottom>
           example project
         </Typography>
-        <Button variant="contained" color="secondary" onClick={this.handleClick}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={this.handleClick}
+        >
           Super Secret Password
         </Button>
       </div>
@@ -71,7 +123,7 @@ class App extends Component {
 }
 
 App.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withRoot(withStyles(styles)(App));
--- src/withRoot.js
+++ src/withRoot.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
+import blue from '@material-ui/core/colors/green';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 // A theme with custom primary and secondary color.
@@ -9,9 +10,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700],
     },
     secondary: {
       light: green[300],
